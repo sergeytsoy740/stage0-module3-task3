bd35c5d063e2a8dae7cf9af7f6f28073144fc109
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-09 20:40:32 +0600

    Module #3. Task #3. Variables (correct)

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -4,7 +4,7 @@ public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
         int first = 1, second = 10, third = 100;
-        System.out.println(first + "\n" + second + "\n" + third);
+     //   System.out.println(first + "\n" + second + "\n" + third);
 
         int linkToFirst = first, linkToSecond = second, linkToThird = third;
         System.out.println(linkToFirst + "\n" + linkToSecond + "\n" + linkToThird);
