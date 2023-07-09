001542c60f8f219795805b295946875e61768cb8
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-09 20:29:52 +0600

    Module #3. Task #3. Variables

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -3,5 +3,8 @@ package school.mjc.stage0.module3.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         //write code here
+        int first = 1, second = 10, third = 100;
+
+        System.out.println(first + "\n" + second + "\n" + third);
     }
 }
